f9d57cadbbf3912386e0db0579e1e7f5b26a5c30
INK-Kano <user1205@naver.com>
2021-05-24 20:05:21 +0900

    Change name

---
+++ c_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main()
+{
+    
+
+    return 0;
+}
