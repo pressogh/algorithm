dd9c7c2eec2f7ebfdfbb5a8f099a201d5cd797b4
INK-Kano <user1205@naver.com>
2021-05-24 16:40:00 +0900

    기본 틀

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+int main()
+{
+    cin.tie(nullptr); cout.tie(nullptr), ios::sync_with_stdio(false);
+
+    
+
+    return 0;
+}
+
+/*
+4
+2 3 1
+5 2 4 1
+
+4
+3 3 4
+1 1 1 1
+
+5
+1 1 1 1
+1 5 4 3 2
+*/
