afff0f40ce3fc6748ce261fccd6d62a2042d21b0
INK-Kano <user1205@naver.com>
2021-05-24 20:02:54 +0900

    4796 캠핑

--- main.cpp
+++ main.cpp
@@ -8,7 +8,25 @@ int main()
 {
     cin.tie(nullptr); cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    cout << "hello" << endl;
+    int l = 1, p, v;
+    int cnt = 1;
+
+    while(1)
+    {
+        ll ans = 0;
+
+        cin >> l >> p >> v; // v일동안 연속하는 p일중 l일 사용가능
+        if (l == 0) break;
+
+        ll temp = 0;
+        if(l > v % p)
+            ans = v % p + v / p * l;
+        else
+            ans = l + v / p * l;
+
+        cout << "Case " << cnt << ": " << ans << endl;
+        cnt++;
+    }
 
     return 0;
 }
