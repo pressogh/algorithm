291731671daf8ce41559150fd5b85d9d90840f19
INK-Kano <user1205@naver.com>
2021-05-29 17:31:33 +0900

    15903 카드 합체 놀이

--- main.cpp
+++ main.cpp
@@ -8,16 +8,33 @@ int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    int n, m;
+    ll n, m;
     cin >> n >> m;
 
-    vector<int> arr;
-    for (int i = 0; i < n; i++)
+    vector<ll> arr;
+    for (auto i = 0; i < n; i++)
     {
-        int temp;
+        ll temp;
         cin >> temp;
         arr.push_back(temp);
     }
+
+    ll ans = 0;
+    while (m--)
+    {
+        sort(arr.begin(), arr.end());
+
+        ll temp = arr[0] + arr[1];
+        arr[0] = temp;
+        arr[1] = temp;
+    }
+
+    for (auto i = 0; i < arr.size(); i++)
+    {
+        ans += arr[i];
+    }
+
+    cout << ans;
 
     return 0;
 }
