e98110fd7c312c9e05329e3bba1b988e0399560d
INK-Kano <user1205@naver.com>
2021-07-13 01:52:45 +0900

    13275 가장 긴 팰린드롬 부분 문자열

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
+// 13275
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 using ld = long double;
 
+string s;
+
+int pal(int left, int right) 
+{
+    while ((left >= 0 && right < s.size()) && s[left] == s[right])
+    {
+        left--;
+        right++;
+    }
+    return right - left - 1;
+}
+
 int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    
+    cin >> s;
+    string temp = s;
+    reverse(s.begin(), s.end());
+
+    if (s == temp || s.size() < 2)
+    {
+        cout << s.size();
+        return 0;
+    }
+
+    int ans = 0;
+    for (int i = 0; i < s.size() - 1; i++)
+    {
+        ans = max({ans, pal(i, i + 1), pal(i, i + 2)});
+    }
+    cout << ans;
+
     return 0;
 }
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-# 2750
-n = int(input())
-lst = []
-while n:
-    temp = int(input())
-    lst.append(temp)
-    n -= 1
+# 13275
+import collections  # 가장 많은 숫자, deque 등
+import sys          # 여러줄 입력
+import re           # 문자 제거
+import string       # 문자열 함수
+import copy         # 깊은 복사
+import itertools    # 순열 조합
+import math         # 수학
+import bisect       # 이진 탐색
 
-print("\n".join(map(str, sorted(lst))))
+s = sys.stdin.readline()
+ls = len(s)
+if ls < 2 or s == s[::-1]:
+    print(ls)
+    exit(0)
+
+def pal(left, right):
+    while left >= 0 and right < ls and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1
+
+ans = 0
+for i in range(ls - 1):
+    ans = max(ans, pal(i, i + 1), pal(i, i + 2))
+print(ans)
