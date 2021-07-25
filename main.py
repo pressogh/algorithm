94a6bd380729da0f87191eb630e993c70e23a9a1
INK-Kano <user1205@naver.com>
2021-07-26 00:22:57 +0900

    15904 UCPC는 무엇의 약자일까?

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 1747
+# 15904
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -8,18 +8,38 @@ import itertools    # 순열 조합(permutations, combinations)
 import math         # 수학
 import bisect       # 이진 탐색
 
-n = int(input())
-tmplst = [x for x in range(1003002)]
-
-for i in range(2, 1003002):
-    if tmplst[i] == 0:
-        continue
-    for j in range(i * 2, 1003002, i):
-        tmplst[j] = 0
-tmplst = [x for x in tmplst if x != 0]
-del tmplst[0]
-
-for item in tmplst:
-    if item >= n and str(item) == str(item)[::-1]:
-        print(item)
+s = sys.stdin.readline()
+t1 = ['U']
+t2 = ['U', 'C']
+t3 = ['U', 'C', 'P']
+t4 = ['U', 'C', 'P', 'C']
+cnt = 0
+tmp = []
+while True:
+    if cnt >= len(s) or tmp == t1:
         break
+    if s[cnt] == 'U':
+        tmp.append(s[cnt])
+    cnt += 1
+while True:
+    if cnt >= len(s) or tmp == t2:
+        break
+    if s[cnt] == 'C':
+        tmp.append(s[cnt])
+    cnt += 1
+while True:
+    if cnt >= len(s) or tmp == t3:
+        break
+    if s[cnt] == 'P':
+        tmp.append(s[cnt])
+    cnt += 1
+while True:
+    if cnt >= len(s) or tmp == t4:
+        break
+    if s[cnt] == 'C':
+        tmp.append(s[cnt])
+    cnt += 1
+if tmp == ['U', 'C', 'P', 'C']:
+    print("I love UCPC")
+else:
+    print("I hate UCPC")
