73c85dcde2f078e1fc68897b0c1a9f4ab570567b
INK-Kano <user1205@naver.com>
2021-07-27 03:51:33 +0900

    1157 단어 공부

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 1037
+# 1157
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -8,7 +8,15 @@ import itertools    # 순열 조합(permutations, combinations)
 import math         # 수학
 import bisect       # 이진 탐색
 
-n = int(input())
-lst = list(map(int, input().split()))
-lst = sorted(lst)
-print(lst[0] * lst[len(lst) - 1])
+s = input()
+s = s.upper()
+
+if len(s) == 1:
+    print(s)
+    exit(0)
+counter = collections.Counter(s)
+counter = sorted(list(zip(counter.keys(), counter.values())), key=lambda x: x[1])
+if counter[len(counter) - 1][1] == counter[len(counter) - 2][1]:
+    print("?")
+else:
+    print(counter[len(counter) - 1][0])
