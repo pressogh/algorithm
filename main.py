4549666ff9e7128b02242b0e9ef3c5aba8f6255c
INK-Kano <user1205@naver.com>
2021-07-28 16:10:09 +0900

    10174 팰린드롬

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 1157
+# 10174
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -8,15 +8,11 @@ import itertools    # 순열 조합(permutations, combinations)
 import math         # 수학
 import bisect       # 이진 탐색
 
-s = input()
-s = s.upper()
-
-if len(s) == 1:
-    print(s)
-    exit(0)
-counter = collections.Counter(s)
-counter = sorted(list(zip(counter.keys(), counter.values())), key=lambda x: x[1])
-if counter[len(counter) - 1][1] == counter[len(counter) - 2][1]:
-    print("?")
-else:
-    print(counter[len(counter) - 1][0])
+n = int(input())
+lst = [input() for _ in range(n)]
+lst = list(map(lambda x: x.upper() == x[::-1].upper(), lst))
+for item in lst:
+    if item == True:
+        print("Yes")
+    else:
+        print("No")
