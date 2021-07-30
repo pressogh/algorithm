e5284969d207e84561418cdc5b9a94116c2eb851
INK-Kano <user1205@naver.com>
2021-07-30 14:30:50 +0900

    1015 수열 정렬

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 10804
+# 1015
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -7,9 +7,13 @@ import copy         # 깊은 복사
 import itertools    # 순열 조합(permutations, combinations)
 import math         # 수학
 import bisect       # 이진 탐색
+import pprint       # 출력
 
-lst = [i for i in range(1, 21)]
-for i in range(10):
-    n, m = map(int, input().split())
-    lst[n-1:m] = lst[n-1:m][::-1]
-print(*lst, end=' ')
+n = int(input())
+lst = list(map(int, input().split()))
+tmp = copy.deepcopy(lst)
+cnt = [0 for _ in range(1001)]
+tmp.sort()
+for i in range(n):
+    print(tmp.index(lst[i]) + cnt[tmp.index(lst[i])], end=' ')
+    cnt[tmp.index(lst[i])] += 1
