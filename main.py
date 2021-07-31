ffec35b5e6670506e9bbb2a63932c495bb68d56f
INK-Kano <user1205@naver.com>
2021-08-01 01:38:42 +0900

    10989 수 정렬하기 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 곳감(모래시계)
+# 10989
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -10,32 +10,11 @@ import bisect       # 이진 탐색
 import pprint       # 출력
 
 n = int(input())
-lst = [collections.deque(map(int, input().split())) for _ in range(n)]
-m = int(input())
-
-for i in range(m):
-    a, b, c = map(int, input().split())
-    a -= 1
-    if b:
-        for j in range(c):
-            lst[a].appendleft(lst[a].pop())
-    else:
-        for j in range(c):
-            lst[a].append(lst[a].popleft())
-
-ans = 0
-left, right = 0, n - 1
-i = 0
-while left <= right:
-    ans += sum(list(itertools.islice(lst[i], left, right+1)))
-    i += 1
-    left += 1
-    right -= 1
-left = n//2-1
-right = n//2+1
-while left >= 0 or right <= n - 1:
-    ans += sum(list(itertools.islice(lst[i], left, right+1)))
-    i += 1
-    left -= 1
-    right += 1
-print(ans)
+lst = [0 for _ in range(10001)]
+for i in range(n):
+    tmp = int(sys.stdin.readline().rstrip())
+    lst[tmp] += 1
+for i in range(10001):
+    if lst[i] != 0:
+        for j in range(lst[i]):
+            print(i)
