82e7a55e96a0985044164e1055f0dbfdfa849ac3
INK-Kano <user1205@naver.com>
2021-07-31 16:13:33 +0900

    격자판 최대합

--- Python/1026 보물.py
+++ Python/1026 보물.py
@@ -1,4 +1,4 @@
-# 격자판 최대합
+# 1026
 import collections  # 가장 많은 숫자, deque 등
 import sys          # 여러줄 입력
 import re           # 문자 제거
@@ -10,24 +10,12 @@ import bisect       # 이진 탐색
 import pprint       # 출력
 
 n = int(input())
-lst = []
+l1 = list(map(int, input().split()))
+l2 = list(map(int, input().split()))
 
-ans = -1
+l1.sort()
+l2.sort(reverse=True)
+ans = 0
 for i in range(n):
-    tmp = list(map(int, input().split()))
-    ans = max(ans, sum(tmp))
-    lst.append(tmp)
-
-for i in range(n):
-    tn = 0
-    for j in range(n):
-        tn += lst[j][i]
-    ans = max(ans, tn)
-
-tn1 = 0
-tn2 = 0
-for i in range(n):
-    tn1 += lst[i][i]
-    tn2 += lst[n - i - 1][n - i - 1]
-ans = max(ans, tn1, tn2)
+    ans += l1[i] * l2[i]
 print(ans)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,57 @@
-// 13275
+// 1038
 #include <bits/stdc++.h>
 
 using namespace std;
 using ll = long long;
 using ld = long double;
 
+bool f(int n)
+{
+    int tmp = -1;
+    while(n)
+    {
+        if (n % 10 > tmp)
+        {
+            tmp = n % 10;
+            n = n / 10;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
+    int n;
+    cin >> n;
+    if (n == 1022)
+    {
+        cout << "9876543210";
+        return 0;
+    }
+
+    if (n > 1022)
+    {
+        cout << -1;
+        return 0;
+    }
+    
+    int cnt = 0, i = 0;
+    while(1)
+    {
+        if (cnt >= n) break;
+        i++;
+        string tmp = to_string(i);
+        if (tmp[0] < tmp.length() - 1) continue;
+        cout << tmp[0] << ' ' << tmp.length() - 1 << endl;
+        if (f(i)) cnt++;
+    }
+    cout << i;
 
     return 0;
 }
