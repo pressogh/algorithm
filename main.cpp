2d39a7cd1fa0c3e586de94b19cb23f893bfe8e7e
INK-Kano <user1205@naver.com>
2021-08-01 02:14:26 +0900

    2581 소수

--- main.cpp
+++ main.cpp
@@ -9,26 +9,7 @@ int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    int n;
-    cin >> n;
-    vector<int> arr(10001, 0);
-
-    for (int i = 0; i < n; i++)
-    {
-        int tmp;
-        cin >> tmp;
-        arr[tmp] += 1;
-    }
-    for (int i = 0; i < 10001; i++)
-    {
-        if (arr[i] != 0)
-        {
-            for (int j = 0; j < arr[i]; j++)
-            {
-                cout << i << '\n';
-            }
-        }
-    }
+    
 
     return 0;
 }
