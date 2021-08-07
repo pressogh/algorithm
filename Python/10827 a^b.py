9b81923ba4b214a9b0808e21ee5f765c74b42f32
INK-Kano <user1205@naver.com>
2021-08-08 01:25:44 +0900

    10827 a^b

---
+++ Python/10827 a^b.py
@@ -0,0 +1,17 @@
+# 10827
+import collections  # 가장 많은 숫자, deque 등
+import sys          # 여러줄 입력
+import re           # 문자 제거
+import string       # 문자열 함수
+import copy         # 깊은 복사
+import itertools    # 순열 조합(permutations, combinations)
+import math         # 수학
+import bisect       # 이진 탐색
+import pprint       # 출력
+from decimal import *
+
+getcontext().prec = 1101
+lst = list(map(str, input().split()))
+n = Decimal(lst[0])
+m = Decimal(lst[1])
+print("{:f}".format(Decimal(pow(n, m))))
