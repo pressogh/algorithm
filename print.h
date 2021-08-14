116587548395acf174e5727068f1abc6974f426f
INK-Kano <user1205@naver.com>
2021-08-14 20:08:15 +0900

    c++ print.h 헤더 추가

---
+++ print.h
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void print(deque<int> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        cout << lst[i] << ' ';
+    }
+}
+void print(vector<int> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        cout << lst[i] << ' ';
+    }
+}
+void print(deque<string> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        cout << lst[i] << ' ';
+    }
+}
+void print(vector<string> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        cout << lst[i] << ' ';
+    }
+}
+
+void print(deque<deque<int>> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        for (int j = 0; j < lst[i].size(); j++)
+        {
+            cout << lst[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
+void print(vector<vector<int>> lst)
+{
+    for (int i = 0; i < lst.size(); i++)
+    {
+        for (int j = 0; j < lst[i].size(); j++)
+        {
+            cout << lst[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
