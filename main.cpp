e645e111d3893e2b5ca51ddc2e7966c4ff7d0e95
INK-Kano <user1205@naver.com>
2021-08-15 03:38:00 +0900

    1057 토너먼트

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //2493
 #include <bits/stdc++.h>
-// #include "print.h"
+#include "print.h"
 
 using namespace std;
 using ll = long long;
@@ -10,26 +10,7 @@ int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    int n;
-    cin >> n;
-
-    deque<pair<int, int>> arr;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-
-        while (!arr.empty()) 
-        {
-            if (arr.back().second > tmp)
-            {
-                cout << arr.back().first << " ";
-                break;
-            }
-            arr.pop_back();
-        }
-        if (arr.empty()) cout << "0 ";
-        arr.push_back(make_pair(i + 1, tmp));
-    }
+    
 
     return 0;
 }
