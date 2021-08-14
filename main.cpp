a12e8014bdbad25b437982cbf2b7db97ae2a2c6a
INK-Kano <user1205@naver.com>
2021-08-15 04:31:55 +0900

    11050 이항 계수 1

--- main.cpp
+++ main.cpp
@@ -10,20 +10,7 @@ int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    int n, a, b;
-    cin >> n >> a >> b;
-
-    int ans = 0;
-    while (1)
-    {
-        ans++;
-        if (a % 2 != 0) a++;
-        if (b % 2 != 0) b++;
-        a /= 2;
-        b /= 2;
-        if (a == b) break;
-    }
-    cout << ans;
+    
 
     return 0;
 }
