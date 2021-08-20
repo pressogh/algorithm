f8d6ed5462c44c3aaab6e28022232e06ddf710dc
pressogh <user1205@naver.com>
2021-08-20 21:55:10 +0900

    ubuntu push test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 9012
+# 
 import collections      # 가장 많은 숫자, deque 등
 import sys              # 여러줄 입력
 import re               # 문자 제거
@@ -11,24 +11,3 @@ import pprint           # 출력
 from decimal import *   # 임의 정밀도
 import random
 
-for _ in range(int(input())):
-    s = input()
-    lst = collections.deque()
-
-    i = 0
-    flag = False
-    while True:
-        if i >= len(s):
-            break
-        if s[i] == '(':
-            lst.append('(')
-        elif s[i] == ')':
-            if not lst:
-                flag = True
-            else:
-                lst.pop()
-        i += 1
-    if flag or lst:
-        print('NO')
-    else:
-        print('YES')
