a37ac6ba5c591e0254dd04bfc797111092d014b8
INK-Kano <user1205@naver.com>
2021-08-24 20:14:50 +0900

    2671 잠수함식별

--- Python/2671 잠수함식별.py
+++ Python/2671 잠수함식별.py
@@ -1,7 +1,7 @@
 # 2671
 import collections      # 가장 많은 숫자, deque 등
 import sys              # 여러줄 입력
-import re               # 문자 제거
+import re               # 문자 제거, 패턴 매칭
 import string           # 문자열 함수
 import copy             # 깊은 복사
 import itertools        # 순열 조합(permutations, combinations)
