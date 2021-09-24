b0cf1a15735c6c530cdaff49ac3a82b93b6c88a5
INK-Kano <user1205@naver.com>
2021-09-24 20:25:56 +0900

    13417 카드 문자열

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 1235
+# 16236
 import collections          # 가장 많은 숫자, deque 등
 import sys                  # 여러줄 입력
 import re                   # 문자 제거
@@ -14,15 +14,40 @@ import heapq                # 우선순위 큐
 import random
 input = sys.stdin.readline
 
-lst = [input().rstrip() for _ in range(int(input()))]
-ans = 0
-for i in range(len(lst)):
-    for j in range(len(lst)):
-        if i != j:
-            tmp = 0
-            for k in range(len(lst[i])-1, -1, -1):
-                if lst[i][k] != lst[j][k]:
-                    break
-                tmp += 1
-            ans = max(tmp, ans)
-print(ans + 1)
+# n = int(input())
+# lst = [list(map(int, input().rstrip().split())) for _ in range(n)]
+
+# feed = [[] for _ in range(40 + 1)]
+
+# def getFeedLength(x, y):
+#     for i in range(n):
+#         for j in range(n):
+#             if not lst[i][j] == 0 and not lst[i][j] == 9:
+#                 feed[abs(x - i) + abs(y - j)].append((i, j, lst[i][j]))
+
+# getFeedLength(2, 2)
+# for item in feed:
+#     item.sort(key=lambda x: (x[0], x[1]))
+# pprint(feed)
+
+# 갈 수 있는 점을 bfs로 탐색하고 그 점까지의 거리를 구한 후 정렬
+
+for _ in range(int(input())):
+    input()
+    lst = list(input().rstrip().split())
+    ans = collections.deque()
+
+    for item in lst:
+        if not ans:
+            ans.append(item)
+            continue
+
+        if item <= ans[-1]:
+            ans.append(item)
+        elif item >= ans[0]:
+            ans.appendleft(item)
+        else:
+            ans.appendleft(item)
+
+    ans = list(ans)
+    print("".join(ans[::-1]))
