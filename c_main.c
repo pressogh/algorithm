e6052eae60696f9b7b4be220557b82e74e7592ac
INK-Kano <user1205@naver.com>
2021-10-08 00:54:22 +0900

    7894 큰 수

--- c_main.c
+++ c_main.c
@@ -5,16 +5,7 @@
 
 int main()
 {
-    int x = 1, y = 3, z = 0;
-    if (x < y && x < z) {
-        printf("minnum: x");
-    }
-    else if (y < x && y < z) {
-        printf("minnum: y");
-    }
-    else {
-        printf("minnum: z");
-    }
+    
 
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//5525
+// 7894
 #include <bits/stdc++.h>
 // #include "print.h"
 
@@ -6,43 +6,23 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
-int stringHash(string s, int n)
-{
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        ans += s[n - 1 - i] * pow(2, i);
-    }
-    return ans;
-}
-
 int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
-    clock_t start, fin;
 
-    int n;
-    while (cin >> n)
-    {
-        vector<int> lst, ans;
-        ans.push_back(-2147000000);
-        for (int i = 0; i < n; i++) {
-            int tmp;
-            cin >> tmp;
-            lst.push_back(tmp);
-        }
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
 
-        for (int i = 0; i < n; i++) {
-            if (lst[i] > ans[ans.size()-1]) {
-                ans.push_back(lst[i]);
-            }
-            int tmp = lower_bound(ans.begin(), ans.end(), lst[i]) - ans.begin();
-            if (lst[i] > ans[tmp - 1] && lst[i] < ans[tmp])
-                ans[tmp] = lst[i];
-        }
-        
-        cout << ans.size() - 1 << '\n';
+        ld tmp = 0;
+        while(n) {
+            tmp += log10(n--);
+        } 
+
+        cout << (int)tmp + 1 << "\n";
     }
     
-    fin = clock();
     return 0;
 }
