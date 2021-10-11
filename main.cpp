1c3793e8410f64291f3e81425d6aef79caaa78d9
INK-Kano <user1205@naver.com>
2021-10-12 01:00:15 +0900

    4358 생태학

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// 7894
+// 1990
 #include <bits/stdc++.h>
 // #include "print.h"
 
@@ -6,23 +6,33 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
+bool isPel(int n) {
+    string tmp = to_string(n);
+    reverse(tmp.begin(), tmp.end());
+
+    if (to_string(n) == tmp) return true;
+    return false;
+}
+
 int main()
 {
     cin.tie(nullptr), cout.tie(nullptr), ios::sync_with_stdio(false);
 
-    ll t;
-    cin >> t;
-    while (t--) {
-        ll n;
-        cin >> n;
+    ll n, m;
+    cin >> n >> m;
 
-        ld tmp = 0;
-        while(n) {
-            tmp += log10(n--);
-        } 
-
-        cout << (int)tmp + 1 << "\n";
+    int lst[m + 1];
+    for (auto i = 0; i < m + 1; i++) lst[i] = i;
+    for (auto i = 2; i < m + 1; i++) {
+        if (lst[i] == 0) continue;
+        for (auto j = i * 2; j < m + 1; j += i) lst[j] = 0;
     }
+    lst[1] = 0;
+
+    for (auto i = n; i < m + 1; i++) {
+        if (isPel(i) and lst[i]) cout << i << "\n";
+    }
+    cout << -1;
     
     return 0;
 }
