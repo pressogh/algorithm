29104a3913c9c4bd034c36ba669dd2230f2c29c3
INK-Kano <user1205@naver.com>
2021-10-14 04:23:05 +0900

    1927 최소 힙

--- c_main.c
+++ c_main.c
@@ -2,10 +2,19 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+int isLeap(int n) {
+    if (n < 1) return 0;
+    printf("%d\n", n);
+    isLeap(n - 1);
+}
 
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    isLeap(n);
 
     return 0;
 }
