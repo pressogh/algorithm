987ad1edaf225c6a5162faf4e80f6ffc78e8959f
PRESSO <user1205@naver.com>
2021-12-29 17:07:03 +0900

    23296 엘리베이터 조작

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 16236
+# 17224
 import collections          # 가장 많은 숫자, deque 등
 import sys                  # 여러줄 입력
 import re                   # 문자 제거
@@ -13,51 +13,25 @@ import functools            # sort key 함수(cmp_to_key)
 import heapq                # 우선순위 큐
 import random
 input = sys.stdin.readline
-
-n = int(input())
-lst = {}
-indegree = {}
-
+    
+n, l, k = map(int, input().split())
+lst = []
 for i in range(n):
-    s1, s2 = input().split()
-    if s1 in lst:
-        lst[s1].append(s2)
-        indegree[s2] += 1
-    else:
-        lst[s1] = [s2]
-        indegree[s1] = 0
-        indegree[s2] = 1
-    if s2 not in lst:
-        lst[s2] = []
+    m1, m2 = map(int, input().split())
+    lst.append((m1, m2))
 
-q = []
-res = []
-for key in indegree.keys():
-    if indegree[key] == 0:
-        q.append([0, key])
-        res.append([0, key])
+lst.sort(key=lambda x: (x[1], x[0]))
+score = 0
+cnt = 0
 
-q = collections.deque(sorted(q))
-res.sort()
+for item in lst:
+    if cnt >= k:
+        break
+    if item[1] <= l:
+        score += 140
+        cnt += 1
+    elif item[0] <= l:
+        score += 100
+        cnt += 1
 
-for i in range(len(indegree.keys())):
-    if not q:
-        print(-1)
-        exit(0)
-
-    q = collections.deque(sorted(list(q)))
-    tmp = q[0]
-    q.popleft()
-
-    t = []
-    for item in lst[tmp[1]]:
-        indegree[item] -= 1
-        tmp[0] += 1
-        if indegree[item] == 0:
-            t.append(item)
-
-    for item in sorted(t):
-        q.append([tmp[0], item])
-        res.append([tmp[0], item])
-
-print(res)
+print(score)
