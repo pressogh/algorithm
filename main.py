fdb5a1b6a76e403dbf0841fbf6098750516dc4c4
PRESSO <user1205@naver.com>
2022-03-21 19:02:27 +0900

    1976 여행 가자

--- main.py
+++ main.py
@@ -11,28 +11,28 @@ from pprint import pprint   # 출력
 from decimal import *       # 임의 정밀도
 import functools            # sort key 함수(cmp_to_key)
 
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-def bfs(start):
-    q = collections.deque()
-    q.append((start, 0))
-    check = [False for _ in range(200002)]
+# def bfs(start):
+#     q = collections.deque()
+#     q.append((start, 0))
+#     check = [False for _ in range(200002)]
 
-    while q:
-        num, cnt = q.popleft()
-        if num == m:
-            print(cnt)
-            exit(0)
+#     while q:
+#         num, cnt = q.popleft()
+#         if num == m:
+#             print(cnt)
+#             exit(0)
 
-        if not check[num + 1] and 0 <= num + 1 <= 100000:
-            q.append((num + 1, cnt + 1))
-            check[num + 1] = True
-        if not check[num - 1] and 0 <= num - 1 <= 100000:
-            q.append((num - 1, cnt + 1))
-            check[num - 1] = True
-        if not check[num * 2] and 0 <= num * 2 <= 100000:
-            q.append((num * 2, cnt + 1))
-            check[num * 2] = True
+#         if not check[num + 1] and 0 <= num + 1 <= 100000:
+#             q.append((num + 1, cnt + 1))
+#             check[num + 1] = True
+#         if not check[num - 1] and 0 <= num - 1 <= 100000:
+#             q.append((num - 1, cnt + 1))
+#             check[num - 1] = True
+#         if not check[num * 2] and 0 <= num * 2 <= 100000:
+#             q.append((num * 2, cnt + 1))
+#             check[num * 2] = True
 
 
-bfs(n)
+# bfs(n)
