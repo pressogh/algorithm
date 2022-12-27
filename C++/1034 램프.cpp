1b3ed1ceaed3950e1b0d067a063b5c0971dbd3ef
PRESSO <caff1nepill@gmail.com>
2022-12-27 14:49:52 +0900

    delete comment

--- C++/1034 램프.cpp
+++ C++/1034 램프.cpp
@@ -55,11 +55,6 @@ int main()
     }
 
     cout << ans << '\n';
-//    for (int i = 0; i < m; i++) {
-//        for (int j = 0; j < n; j++) {
-//            cout << arr[i][j] << ' ';
-//        }
-//        cout << '\n';
-//    }
+
     return 0;
 }
