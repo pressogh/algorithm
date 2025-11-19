9bb22e583a4459b3b847e25c50448832938017a2
PRESSO <caff1nepill@gmail.com>
2025-11-19 13:14:49 +0900

    solve: 1764 듣보잡

--- python/solved/1764 듣보잡.py
+++ python/solved/1764 듣보잡.py
@@ -1,26 +1,15 @@
-# 1764
-import collections      # 가장 많은 숫자, deque 등
-import sys              # 여러줄 입력
-import re               # 문자 제거
-import string           # 문자열 함수
-import copy             # 깊은 복사
-import itertools        # 순열 조합(permutations, combinations)
-import math             # 수학
-import bisect           # 이진 탐색
-import pprint           # 출력
-from decimal import *   # 임의 정밀도
+import sys, os, io, atexit
 
-n, m = map(int, input().split())
-lst = {}
-ans = []
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-for i in range(n):
-    lst[sys.stdin.readline().rstrip()] = 1
-for i in range(m):
-    tmp = sys.stdin.readline().rstrip()
-    if lst.get(tmp):
-        ans.append(tmp)
-ans.sort()
-print(len(ans))
-for i in range(len(ans)):
-    print(ans[i])
+def solve():
+    n, m = map(int, input().split())
+    s = set(input() for _ in range(n)) & set(input() for _ in range(m))
+    print(len(s))
+    print(*sorted(s), sep='\n')
+
+if __name__ == '__main__':
+    solve()
