7087726a9ec65e7bb806612751bb3df7f8c0b5ee
PRESSO <caff1nepill@gmail.com>
2025-11-19 13:45:48 +0900

    solve: 11399 ATM

--- python/solved/11399 ATM.py
+++ python/solved/11399 ATM.py
@@ -1,22 +1,14 @@
-# 11399
-import collections  # 가장 많은 숫자, deque 등
-import sys          # 여러줄 입력
-import re           # 문자 제거
-import string       # 문자열 함수
-import copy         # 깊은 복사
-import itertools    # 순열 조합(permutations, combinations)
-import math         # 수학
-import bisect       # 이진 탐색
-import pprint       # 출력
+import sys, os, io, atexit
 
-n = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-ans = 0
-for i in range(n):
-    tmp = 0
-    for j in range(i+1):
-        tmp += lst[j]
-    ans += tmp
-print(ans)
+def solve():
+    input()
+    l = sorted([(x, i) for i, x in enumerate(map(int, input().split()))])
+    print(sum(x[0] * (len(l) - i) for i, x in enumerate(l)))
+
+if __name__ == '__main__':
+    solve()
