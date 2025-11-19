5aba3d004111fbd2e8df1c9c8bec7f18a9e009a7
PRESSO <caff1nepill@gmail.com>
2025-11-19 14:16:19 +0900

    solve: 1463 1로 만들기

--- python/solved/1463 1로 만들기.py
+++ python/solved/1463 1로 만들기.py
@@ -1,22 +1,19 @@
-# 1463
-import collections      # 가장 많은 숫자, deque 등
-import sys              # 여러줄 입력
-import re               # 문자 제거
-import string           # 문자열 함수
-import copy             # 깊은 복사
-import itertools        # 순열 조합(permutations, combinations)
-import math             # 수학
-import bisect           # 이진 탐색
-import pprint           # 출력
-from decimal import *   # 임의 정밀도
+import sys, os, io, atexit
 
-n = int(input())
-lst = [0 for i in range(n+1)]
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-for i in range(2, n+1):
-    lst[i] = lst[i-1] + 1
-    if i % 3 == 0:
-        lst[i] = min(lst[i], lst[i // 3] + 1)
-    if i % 2 == 0:
-        lst[i] = min(lst[i], lst[i // 2] + 1)
-print(lst[n])
+def solve():
+    n = int(input())
+    dp = [1000000] * (n + 1)
+    dp[0], dp[1] = 0, 0
+    for i in range(2, n + 1):
+        if i % 3 == 0: dp[i] = min(dp[i], dp[i // 3] + 1)
+        if i % 2 == 0: dp[i] = min(dp[i], dp[i // 2] + 1)
+        dp[i] = min(dp[i], dp[i - 1] + 1)
+    print(dp[-1])
+
+if __name__ == '__main__':
+    solve()
