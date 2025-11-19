4f2b2c14d7a8362a3d768068585e8f28e4c9d26c
PRESSO <caff1nepill@gmail.com>
2025-11-19 14:02:48 +0900

    solve: 1003 피보나치 함수

--- python/solved/1003 피보나치 함수.py
+++ python/solved/1003 피보나치 함수.py
@@ -1,19 +1,19 @@
-# 1003
-import collections  # 가장 많은 숫자, deque 등
-import sys          # 여러줄 입력
-import re           # 문자 제거
-import string       # 문자열 함수
-import copy         # 깊은 복사
-import itertools    # 순열 조합(permutations, combinations)
-import math         # 수학
-import bisect       # 이진 탐색
-import pprint       # 출력
+import sys, os, io, atexit
 
-t = int(input())
-while t:
-    tlst = [(1, 0), (0, 1), (1, 1)]
-    n = int(input())
-    for i in range(3, n+1):
-        tlst.append((tlst[i-1][0]+tlst[i-2][0], tlst[i-1][1]+tlst[i-2][1]))
-    print(tlst[n][0], tlst[n][1])
-    t -= 1
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+def solve():
+    dp = [(1, 0), (0, 1)]
+    for _ in range(2, 40 + 1):
+        dp.append((dp[-1][0] + dp[-2][0], dp[-1][1] + dp[-2][1]))
+
+    t = int(input())
+    while t > 0:
+        print(*dp[int(input())])
+        t -= 1
+
+if __name__ == '__main__':
+    solve()
