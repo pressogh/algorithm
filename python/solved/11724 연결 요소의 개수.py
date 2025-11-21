68d5cb72d05b548ea7e39cb2566300213d7459fa
PRESSO <caff1nepill@gmail.com>
2025-11-21 14:06:59 +0900

    solve: 11724 연결 요소의 개수

--- python/solved/11724 연결 요소의 개수.py
+++ python/solved/11724 연결 요소의 개수.py
@@ -1,47 +1,35 @@
-# 11724
-import collections          # 가장 많은 숫자, deque 등
-import sys                  # 여러줄 입력
-import re                   # 문자 제거
-import string               # 문자열 함수
-import copy                 # 깊은 복사
-import itertools            # 순열 조합(permutations, combinations)
-import math                 # 수학
-import bisect               # 이진 탐색
-from pprint import pprint   # 출력
-from decimal import *       # 임의 정밀도
-import functools            # sort key 함수(cmp_to_key)
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from collections import deque
 
 n, m = map(int, input().split())
-if m == 0:
-    print(n)
-    exit(0)
-check = [0 for _ in range(n + 1)]
-lst = [[] for _ in range(n + 1)]
+l = [[False] * (n + 1) for _ in range(n + 1)]
+for _ in range(m):
+    node1, node2 = map(int, input().split())
+    l[node1][node2], l[node2][node1] = True, True
 
-cnt = 2
-def bfs(start):
-    q = collections.deque()
-    q.append(start)
-    check[start] = cnt
+for i in range(1, n + 1):
+    l[i][i] = i
 
-    while q:
-        tmp = q.popleft()
-        for i in range(len(lst[tmp])):
-            if check[lst[tmp][i]] == 0:
-                q.append(lst[tmp][i])
-                check[lst[tmp][i]] = cnt
+memo = [False for _ in range(n + 1)]
+dq = deque([(i, i) for i in range(1, len(l))])
 
-for i in range(m):
-    a, b = map(int, input().split())
-    lst[a].append(b)
-    lst[b].append(a)
+res = set()
+while dq:
+    node, c = dq.pop()
 
-ans = 0
-for i in range(n):
-    if check[i] == 0 and lst[i]:
-        bfs(i)
-        cnt += 1
-ans += max(check)
-check = [item for item in check if item == 0]
-ans += len(check)
-print(ans - 2)
+    if memo[node]: continue
+    memo[node] = True
+    
+    for i in range(len(l[node])):
+        if l[node][i] == True:
+            dq.append((i, c))
+            l[node][i], l[i][node] = c, c
+        res.add(c)
+
+print(len(res))
