e5b1d4874945f8d5111a0ab618ecf1843589e211
PRESSO <caff1nepill@gmail.com>
2025-11-21 11:55:33 +0900

    solve: 1927 최소 힙

--- python/solved/1927 최소 힙.py
+++ python/solved/1927 최소 힙.py
@@ -1,26 +1,14 @@
-# 1927
-import collections          # 가장 많은 숫자, deque 등
-import sys                  # 여러줄 입력
-import re                   # 문자 제거
-import string               # 문자열 함수
-import copy                 # 깊은 복사
-import itertools            # 순열 조합(permutations, combinations)
-import math                 # 수학
-import bisect               # 이진 탐색
-from pprint import pprint   # 출력
-from decimal import *       # 임의 정밀도
-import functools            # sort key 함수(cmp_to_key)
-import heapq                # 우선순위 큐
-import random
-input = sys.stdin.readline
+import sys, os, io, atexit
 
-lst = []
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+import heapq
+
+heap = []
 for _ in range(int(input())):
-    n = int(input())
-    if n == 0:
-        if not lst:
-            print(0)
-        else:
-            print(heapq.heappop(lst))
-    else:
-        heapq.heappush(lst, n)
+    x = int(input())
+    if x == 0: print(0 if not heap else heapq.heappop(heap))
+    else: heapq.heappush(heap, x)
