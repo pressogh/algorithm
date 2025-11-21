280fea7e617316b3b6585e97faac44ff93022719
PRESSO <caff1nepill@gmail.com>
2025-11-21 11:14:20 +0900

    solve: 1260 DFS와 BFS

--- python/solved/1260 DFS와 BFS.py
+++ python/solved/1260 DFS와 BFS.py
@@ -1,64 +1,58 @@
-# 1260
-import collections      # 가장 많은 숫자, deque 등
-import sys              # 여러줄 입력
-import re               # 문자 제거
-import string           # 문자열 함수
-import copy             # 깊은 복사
-import itertools        # 순열 조합(permutations, combinations)
-import math             # 수학
-import bisect           # 이진 탐색
-import pprint           # 출력
-from decimal import *   # 임의 정밀도
-import random
-import functools        # sort key 함수(cmp_to_key)
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from collections import deque
+
+def dfs(l, v):
+    dq = deque([v])
+    check = [[False] * (len(l)) for _ in range(len(l))]
+
+    memo = [False for _ in range(len(l))]
+    res = []
+    while dq:
+        node = dq.pop()
+        if memo[node]: continue
+
+        res.append(node)
+        memo[node] = True
+    
+        for i in range(len(l[node])):
+            to = len(l[node]) - i - 1
+            if l[node][to] and not check[node][to]:
+                check[node][to], check[to][node] = True, True
+                dq.append(to)
+
+    return res
+
+def bfs(l, v):
+    dq = deque([v])
+    check = [[False] * (len(l)) for _ in range(len(l))]
+
+    memo = [False for _ in range(len(l))]
+    res = []
+    while dq:
+        node = dq.popleft()
+        if memo[node]: continue
+        
+        res.append(node)
+        memo[node] = True
+
+        for i in range(len(l[node])):
+            if l[node][i] and not check[node][i]:
+                check[node][i], check[i][node] = True, True
+                dq.append(i)
+    return res
 
 n, m, v = map(int, input().split())
-lst = [[] for _ in range(n + 1)]
+l = [[0] * (n + 1) for _ in range(n + 1)]
 
-def dfs(lst, start):
-    check = [False for _ in range(n + 1)]
-    q = collections.deque()
-    print(start, end=" ")
+for _ in range(m):
+    c1, c2 = map(int, input().split())
+    l[c1][c2], l[c2][c1] = 1, 1
 
-    for i in range(len(lst[start])):
-        q.append(lst[start][i])
-    check[start] = True
-    
-    while q:
-        tmp = q.pop()
-        if not check[tmp]:
-            print(tmp, end=" ")
-            for i in range(len(lst[tmp])):
-                q.append(lst[tmp][i])
-                check[tmp] = True
-    print('')
-
-def bfs(lst, start):
-    check = [False for _ in range(n + 1)]
-    q = collections.deque()
-    print(start, end=" ")
-
-    for i in range(len(lst[start])):
-        q.append(lst[start][i])
-    check[start] = True
-
-    while q:
-        tmp = q.popleft()
-        if not check[tmp]:
-            print(tmp, end=" ")
-            for i in range(len(lst[tmp])):
-                q.append(lst[tmp][i])
-                check[tmp] = True
-    print('')
-
-for i in range(m):
-    a, b = map(int, input().split())
-    lst[a].append(b)
-    lst[b].append(a)
-
-for i in range(len(lst)):
-    lst[i].sort(reverse=True)
-dfs(lst, v)
-for i in range(len(lst)):
-    lst[i].sort()
-bfs(lst, v)
+print(*dfs(l, v))
+print(*bfs(l, v))
