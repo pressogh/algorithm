060bbcdf2a2edf05f9cfe92384c46aa048b0102d
PRESSO <caff1nepill@gmail.com>
2025-11-21 11:38:33 +0900

    solve: 1541 잃어버린 괄호

--- python/solved/1541 잃어버린 괄호.py
+++ python/solved/1541 잃어버린 괄호.py
@@ -1,33 +1,19 @@
-# 1541
-import collections  # 가장 많은 숫자, deque 등
-import sys          # 여러줄 입력
-import re           # 문자 제거
-import string       # 문자열 함수
-import copy         # 깊은 복사
-import itertools    # 순열 조합(permutations, combinations)
-import math         # 수학
-import bisect       # 이진 탐색
-import pprint       # 출력
+import sys, os, io, atexit
 
-s = list(input())
-s.append("*")
-ans = 0
-start = 0
-lst = []
-for i in range(len(s)):
-    if s[i] == '+' or s[i] == '-':
-        lst.append(int("".join(s[start:i])))
-        lst.append(s[i])
-        start = i + 1
-    elif s[i] == "*":
-        lst.append(int("".join(s[start:i])))
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-flag = True
-for i in range(len(lst)):
-    if lst[i] == '-':
-        flag = False
-    if lst[i] != '-' and lst[i] != '+' and flag:
-        ans += lst[i]
-    elif lst[i] != '-' and lst[i] != '+' and not flag:
-        ans -= lst[i]
-print(ans)
+import re
+
+s = re.split(r'(\+|\-)', input())
+
+res = [0]
+for item in s:
+    match item:
+        case '+': pass
+        case '-': res.append(0)
+        case _: res[-1] += int(item)
+
+print(res[0] - sum(res[1:]))
