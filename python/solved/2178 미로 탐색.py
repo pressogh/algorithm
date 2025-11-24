060bbed01885e3f5ebaf67299a6a0ec64e8a66c6
PRESSO <caff1nepill@gmail.com>
2025-11-24 11:20:33 +0900

    solve: 2178 미로 탐색

--- python/solved/2178 미로 탐색.py
+++ python/solved/2178 미로 탐색.py
@@ -1,41 +1,34 @@
-# 2178
-import collections      # 가장 많은 숫자, deque 등
-import sys              # 여러줄 입력
-import re               # 문자 제거
-import string           # 문자열 함수
-import copy             # 깊은 복사
-import itertools        # 순열 조합(permutations, combinations)
-import math             # 수학
-import bisect           # 이진 탐색
-import pprint           # 출력
-from decimal import *   # 임의 정밀도
-import random
-import functools        # sort key 함수(cmp_to_key)
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from collections import deque
+
+dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
 
 n, m = map(int, input().split())
-lst = [list(input()) for _ in range(n)]
+l = [[int(x) for x in input()] for _ in range(n)]
+check = [[False] * m for _ in range(n)]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dq = deque()
+dq.append((0, 0, 0))
+check[0][0] = True
 
-def isSafe(x, y):
-    if 0 <= x < n and 0 <= y < m:
-        return True
-    return False
+res = n * m + 1
+while dq:
+    y, x, cnt = dq.popleft()
 
-def bfs():
-    q = collections.deque()
-    q.append((0, 0))
-    lst[0][0] = '2'
+    if y == (n - 1) and x == (m - 1):
+        res = min(res, cnt)
+        continue
 
-    while q:
-        tmp = q.popleft()
-        for i in range(4):
-            x = tmp[0] + dx[i]
-            y = tmp[1] + dy[i]
-            if isSafe(x, y) and lst[x][y] == '1':
-                lst[x][y] = str(int(lst[tmp[0]][tmp[1]]) + 1)
-                q.append((x, y))
+    for i in range(4):
+        next_y, next_x = dy[i] + y, dx[i] + x
+        if 0 <= next_y < n and 0 <= next_x < m and l[next_y][next_x] and not check[next_y][next_x]:
+            dq.append((next_y, next_x, cnt + 1))
+            check[next_y][next_x] = True
 
-bfs()
-print(int(lst[n - 1][m - 1]) - 1)
+print(res + 1)
