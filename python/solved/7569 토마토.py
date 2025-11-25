a4dfafb5c69929f23693c06283db27c00be17463
PRESSO <caff1nepill@gmail.com>
2025-11-25 16:42:16 +0900

    solve: 7569 토마토

--- python/solved/7569 토마토.py
+++ python/solved/7569 토마토.py
@@ -1,58 +1,39 @@
-# 7569
-import collections          # 가장 많은 숫자, deque 등
-import sys                  # 여러줄 입력
-import re                   # 문자 제거
-import string               # 문자열 함수
-import copy                 # 깊은 복사
-import itertools            # 순열 조합(permutations, combinations)
-import math                 # 수학
-import bisect               # 이진 탐색
-from pprint import pprint   # 출력
-from decimal import *       # 임의 정밀도
-import functools            # sort key 함수(cmp_to_key)
-import heapq                # 우선순위 큐
-import random
-input = sys.stdin.readline
+import sys, os, io, atexit
 
-dx = [0, 1, 0, -1, 0, 0]
-dy = [1, 0, -1, 0, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
-def isSafe(x, y, z):
-    if 0 <= x < h and 0 <= y < m and 0 <= z < n:
-        return True
-    return False
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-def findAns(lst):
-    ans = -1;
-    for i in range(len(lst)):
-        for j in range(len(lst[i])):
-            ans = max(ans, max(lst[i][j]))
-            for k in range(len(lst[i][j])):
-                if lst[i][j][k] == 0:
-                    return 0
-    return ans
+from collections import deque
 
-def bfs():
-    while q:
-        tmp = q.popleft()
-        for i in range(6):
-            nx = tmp[0] + dx[i]
-            ny = tmp[1] + dy[i]
-            nz = tmp[2] + dz[i]
-            if isSafe(nx, ny, nz):
-                if lst[nx][ny][nz] == 0:
-                    lst[nx][ny][nz] = lst[tmp[0]][tmp[1]][tmp[2]] + 1
-                    q.append((nx, ny, nz))
+dz, dy, dx = [1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]
 
-n, m, h = map(int, input().split())
-lst = [[list(map(int, input().rstrip().split())) for _ in range(m)] for _ in range(h)]
+m, n, h = map(int, input().split())
+box = [[[int(x) for x in input().split()] for _ in range(n)] for _ in range(h)]
 
-q = collections.deque()
-for i in range(n):
-    for j in range(m):
-        for k in range(h):
-            if lst[k][j][i] == 1:
-                q.append((k, j, i))
+dq = deque()
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if box[k][i][j] == 1: dq.append(((k, i, j), 1))
 
-bfs()
-print(findAns(lst) - 1)
+while dq:
+    coor, count = dq.popleft()
+
+    for i in range(6):
+        next_z, next_y, next_x, next_count = coor[0] + dz[i], coor[1] + dy[i], coor[2] + dx[i], count + 1
+        if 0 <= next_z < h and 0 <= next_y < n and 0 <= next_x < m and box[next_z][next_y][next_x] == 0:
+            box[next_z][next_y][next_x] = next_count
+            dq.append(((next_z, next_y, next_x), next_count))
+
+res = 0
+for k in range(h):
+    for i in range(n):
+        for j in range(m):
+            if box[k][i][j] == 0:
+                print(-1)
+                exit(0)
+            if res < box[k][i][j]: res = box[k][i][j]
+
+print(res - 1)
