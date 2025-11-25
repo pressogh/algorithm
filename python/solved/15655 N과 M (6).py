a974bf164d07ca9eb8a5658fb3160e7661e637d9
PRESSO <caff1nepill@gmail.com>
2025-11-25 21:47:06 +0900

    solve: 15655 N과 M (6)

---
+++ python/solved/15655 N과 M (6).py
@@ -0,0 +1,23 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from collections import deque
+
+n, m = map(int, input().split())
+l = sorted(list(set(map(int, input().split()))))
+dq = deque([([x], set([x])) for x in l])
+
+while dq:
+    now, used = dq.popleft()
+
+    if len(now) >= m:
+        print(*now)
+        continue
+
+    for i in range(n):
+        if l[i] not in used and l[i] > now[-1]:
+            dq.append((now + [l[i]], used | set([l[i]])))
