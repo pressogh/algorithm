dde606e0d28a4aef78bd83bdcb4df3de5c4a9dec
PRESSO <caff1nepill@gmail.com>
2025-11-26 12:40:47 +0900

    solve: 11055 가장 큰 증가하는 부분 수열

---
+++ python/solved/11055 가장 큰 증가하는 부분 수열.py
@@ -0,0 +1,14 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n = int(input())
+l = [int(x) for x in input().split()]
+
+dp = [0] * (1000 + 1)
+for item in l:
+    dp[item] = max(dp[:item]) + item
+print(max(dp))
