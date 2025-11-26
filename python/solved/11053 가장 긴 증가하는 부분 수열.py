218c1acd833b29d8181c67c0372735fb05ced7de
PRESSO <caff1nepill@gmail.com>
2025-11-26 12:42:15 +0900

    solve: 11053 가장 긴 증가하는 부분 수열

--- python/solved/11053 가장 긴 증가하는 부분 수열.py
+++ python/solved/11053 가장 긴 증가하는 부분 수열.py
@@ -1,27 +1,14 @@
-# 11053
-import collections      # 가장 많은 숫자, deque 등
-import sys              # 여러줄 입력
-import re               # 문자 제거
-import string           # 문자열 함수
-import copy             # 깊은 복사
-import itertools        # 순열 조합(permutations, combinations)
-import math             # 수학
-import bisect           # 이진 탐색
-import pprint           # 출력
-from decimal import *   # 임의 정밀도(getcontext().prec, Decimal)
-import random
-import functools        # sort key 함수(cmp_to_key)
+import sys, os, io, atexit
 
-input()
-lst = list(map(int, input().split()))
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-ans = [-1]
-for i in range(len(lst)):
-    if max(ans) < lst[i]:
-        ans.append(lst[i])
-    if ans:
-        for j in range(1, len(ans)):
-            if lst[i] > ans[j - 1] and lst[i] < ans[j]:
-                ans[j] = lst[i]
-                break
-print(len(ans) - 1)
+n = int(input())
+l = [int(x) for x in input().split()]
+
+dp = [0] * (1000 + 1)
+for item in l:
+    dp[item] = max(dp[:item]) + 1
+print(max(dp))
