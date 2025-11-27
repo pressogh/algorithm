7b433a13c8a3b236022de354253d1027027157a6
PRESSO <caff1nepill@gmail.com>
2025-11-27 09:56:52 +0900

    solve: 1932 정수 삼각형

--- python/solved/1932 정수 삼각형.py
+++ python/solved/1932 정수 삼각형.py
@@ -1,24 +1,20 @@
-# 1932
-import collections          # 가장 많은 숫자, deque 등
-import sys                  # 여러줄 입력
-import re                   # 문자 제거
-import string               # 문자열 함수
-import copy                 # 깊은 복사
-import itertools            # 순열 조합(permutations, combinations)
-import math                 # 수학
-import bisect               # 이진 탐색
-from pprint import pprint   # 출력
-from decimal import *       # 임의 정밀도
-import functools            # sort key 함수(cmp_to_key)
-import heapq                # 우선순위 큐
-import random
-input = sys.stdin.readline
+import sys, os, io, atexit
 
-lst = [list(map(int, input().split())) for _ in range(int(input()))]
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-for i in range(1, len(lst)):
-    lst[i][0] += lst[i - 1][0]
-    lst[i][len(lst[i]) - 1] += lst[i - 1][len(lst[i - 1]) - 1]
-    for j in range(1, len(lst[i]) - 1):
-        lst[i][j] += max(lst[i - 1][j - 1], lst[i - 1][j])
-print(max(lst[-1]))
+n = int(input())
+tri = [[int(x) for x in input().split()] for _ in range(n)]
+dp = [[0] * (i + 1) for i in range(n)]
+
+dp[0][0] = tri[0][0]
+
+for i in range(1, n):
+    for j in range(len(tri[i])):
+        if j == 0: dp[i][j] = dp[i - 1][0] + tri[i][j]
+        elif j == (len(tri[i]) - 1): dp[i][j] = dp[i - 1][-1] + tri[i][j]
+        else: dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + tri[i][j]
+
+print(max(dp[-1]))
