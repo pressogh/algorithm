a422a7b76a9aa5d6314851560856d8d4f20d8af9
PRESSO <caff1nepill@gmail.com>
2025-11-27 09:26:14 +0900

    solve: 16953 A → B

--- python/solved/16953 A → B.py
+++ python/solved/16953 A → B.py
@@ -1,27 +1,28 @@
-# 16953
-import collections  # 가장 많은 숫자, deque 등
-import sys          # 여러줄 입력
-import re           # 문자 제거
-import string       # 문자열 함수
-import copy         # 깊은 복사
-import itertools    # 순열 조합(permutations, combinations)
-import math         # 수학
-import bisect       # 이진 탐색
-import pprint       # 출력
+import sys, os, io, atexit
 
-n, m = map(int, input().split())
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-def bfs(k):
-    q = collections.deque()
-    q.append((k, 1))
-    
-    while q:
-        tmp = q.popleft()
-        if tmp[0] == m:
-            return tmp[1]
-        if tmp[0] > m: 
-            continue
-        q.append((tmp[0] * 2, tmp[1]+1))
-        q.append((tmp[0] * 10 + 1, tmp[1]+1))
-    return -1
-print(bfs(n))
+from collections import deque
+
+a, b = map(int, input().split())
+
+dq = deque([(a, 0)])
+check = set()
+while dq:
+    now, count = dq.popleft()
+    if now == b:
+        print(count + 1)
+        exit(0)
+
+    n1, n2 = now * 2, int(str(now) + "1")
+    if n1 not in check and n1 <= b:
+        check.add(n1)
+        dq.append((n1, count + 1))
+    if n2 not in check and n2 <= b:
+        check.add(n2)
+        dq.append((n2, count + 1))
+
+print(-1)
