fcef4bdbded3621f458cec2d31685566d42eb3a9
PRESSO <caff1nepill@gmail.com>
2025-11-28 17:54:37 +0900

    solve: 5639 이진 검색 트리

--- python/solved/5639 이진 검색 트리.py
+++ python/solved/5639 이진 검색 트리.py
@@ -48,3 +48,34 @@ while True:
         break
 
 tree.post(tree.root)
+
+
+"""
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+sys.setrecursionlimit(10 ** 6)
+
+tree = [int(x) for x in sys.stdin.readlines()]
+
+def post(arr):
+    if len(arr) == 0: return
+
+    l, r = [], []
+    root = arr[0]
+    for i in range(1, len(arr)):
+        if root < arr[i]:
+            l = arr[1:i]
+            r = arr[i:]
+            break
+    else: l = arr[1:]
+
+    post(l)
+    post(r)
+    print(root)
+
+post(tree)
+"""
