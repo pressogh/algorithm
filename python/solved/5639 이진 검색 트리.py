271798b7e345e21a102578e1381f3d8c663b38d0
PRESSO <caff1nepill@gmail.com>
2025-11-28 17:43:32 +0900

    solve: 5639 이진 검색 트리

--- python/solved/5639 이진 검색 트리.py
+++ python/solved/5639 이진 검색 트리.py
@@ -4,7 +4,7 @@ input = lambda: sys.stdin.readline().rstrip('\r\n')
 stdout = io.BytesIO()
 sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 9)
 
 class Node:
     value = None
