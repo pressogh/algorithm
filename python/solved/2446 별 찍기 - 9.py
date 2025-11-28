61279792e27e8c3929fedc7bf755be2532799362
PRESSO <caff1nepill@gmail.com>
2025-11-28 10:08:15 +0900

    solve: 2446 별 찍기 - 9

---
+++ python/solved/2446 별 찍기 - 9.py
@@ -0,0 +1,12 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n = int(input())
+for i in range(n):
+    print(' ' * i + '*' * ((n * 2) - (2 * i) - 1))
+for i in range(1, n):
+    print(' ' * (n - i - 1) + '*' * (2 * i + 1))
