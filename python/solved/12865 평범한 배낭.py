18457a317d2502f15f26315a025d8eaf4f326f50
PRESSO <caff1nepill@gmail.com>
2025-11-30 12:00:09 +0900

    solve: 12865 평범한 배낭

---
+++ python/solved/12865 평범한 배낭.py
@@ -0,0 +1,20 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+sys.setrecursionlimit(10 ** 6)
+
+n, k = map(int, input().split())
+goods = [[0, 0]] + [[int(x) for x in input().split()] for _ in range(n)]
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        w, v = goods[i]
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+print(dp[-1][-1])
