f85413a1c805f099a5551359abc91553186010dd
PRESSO <caff1nepill@gmail.com>
2025-12-05 15:22:54 +0900

    solve: 15624 피보나치 수 7

---
+++ python/solved/15624 피보나치 수 7.py
@@ -0,0 +1,31 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+MOD = 1000000007
+
+def mat_mul(a, b):
+    res = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                res[i][k] += (a[i][j] * b[j][k]) % MOD
+    return res
+
+n = int(input()) - 1
+if n == -1:
+    print(0)
+    exit(0)
+
+mat = [[1, 1], [1, 0]]
+res = [[1, 1], [1, 0]]
+while n > 0:
+    if n % 2 != 0: res = mat_mul(res, mat)
+
+    mat = mat_mul(mat, mat)
+    n //= 2
+
+print(res[0][1] % MOD)
