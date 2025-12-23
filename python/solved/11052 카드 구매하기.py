5a96c88697a5fab49675836bb0b53719b189e493
PRESSO <caff1nepill@gmail.com>
2025-12-23 11:34:06 +0900

    solve: 11052 카드 구매하기

---
+++ python/solved/11052 카드 구매하기.py
@@ -0,0 +1,15 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n = int(input())
+packs = [int(x) for x in input().split()]
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], packs[j - 1] + dp[i - j])
+print(dp[-1])
