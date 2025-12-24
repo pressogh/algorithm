98ae789c207668e2a03f42fd2120919ec921a857
PRESSO <caff1nepill@gmail.com>
2025-12-24 10:35:23 +0900

    solve: 34346 대각선

---
+++ python/solved/34346 대각선.py
@@ -0,0 +1,8 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+print(2 if int(input()) % 2 == 0 else 1)
