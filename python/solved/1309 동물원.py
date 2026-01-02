f38d008b795e8fccccfb4eec9379a5ba6c235b8a
PRESSO <caff1nepill@gmail.com>
2026-01-02 14:09:02 +0900

    solve: 1309 동물원

---
+++ python/solved/1309 동물원.py
@@ -0,0 +1,19 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+MOD = 9901
+
+n = int(input())
+dp = [[0, 0, 0] for _ in range(2)]
+dp[1][0] = 1
+for i in range(1, n + 1):
+    for j in range(3): dp[0][j] = dp[1][j]
+
+    dp[1][0] = dp[0][0] + dp[0][1] + dp[0][2]
+    dp[1][1] = dp[0][0] + dp[0][2]
+    dp[1][2] = dp[0][0] + dp[0][1]
+print(sum(dp[-1]) % MOD)
