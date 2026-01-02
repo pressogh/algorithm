88a4395c45b11b3372f65fbd867d5390fccc54b5
PRESSO <caff1nepill@gmail.com>
2026-01-02 14:10:37 +0900

    solve: 1309 동물원

--- python/solved/1309 동물원.py
+++ python/solved/1309 동물원.py
@@ -13,7 +13,7 @@ dp[1][0] = 1
 for i in range(1, n + 1):
     for j in range(3): dp[0][j] = dp[1][j]
 
-    dp[1][0] = dp[0][0] + dp[0][1] + dp[0][2]
-    dp[1][1] = dp[0][0] + dp[0][2]
-    dp[1][2] = dp[0][0] + dp[0][1]
+    dp[1][0] = (dp[0][0] + dp[0][1] + dp[0][2]) % MOD
+    dp[1][1] = (dp[0][0] + dp[0][2]) % MOD
+    dp[1][2] = (dp[0][0] + dp[0][1]) % MOD
 print(sum(dp[-1]) % MOD)
