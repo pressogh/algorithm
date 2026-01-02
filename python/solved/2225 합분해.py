540ce44c212550e7e81b2e96ca98df7da4a2f318
PRESSO <caff1nepill@gmail.com>
2026-01-02 14:13:47 +0900

    solve: 2225 합분해

--- python/solved/2225 합분해.py
+++ python/solved/2225 합분해.py
@@ -6,6 +6,7 @@ sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 MOD = 1000000000
+
 n, k = map(int, input().split())
 dp = [[0] * (n + 1) for _ in range(k + 1)]
 for i in range(n + 1): dp[1][i] = 1
@@ -14,6 +15,6 @@ for i in range(2, k + 1):
     for j in range(n + 1):
         k = 0
         while j + k <= n:
-            dp[i][j + k] += dp[i - 1][j]
+            dp[i][j + k] = (dp[i][j + k] + dp[i - 1][j]) % MOD
             k += 1
-print(dp[-1][-1] % MOD)
+print(dp[-1][-1])
