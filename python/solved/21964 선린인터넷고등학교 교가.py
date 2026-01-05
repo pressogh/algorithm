ffeb831e951b5f512e8ef1b9135b20ec40f74f37
PRESSO <caff1nepill@gmail.com>
2026-01-05 16:25:55 +0900

    solve: 21964 선린인터넷고등학교 교가

---
+++ python/solved/21964 선린인터넷고등학교 교가.py
@@ -0,0 +1,9 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+_, s = input(), input()
+print(s[-5:])
