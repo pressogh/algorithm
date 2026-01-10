9ee1bf60947269fee810429f7658fa506c922dec
PRESSO <caff1nepill@gmail.com>
2026-01-10 19:41:39 +0900

    solve: 9658 돌 게임 4

---
+++ python/solved/9658 돌 게임 4.py
@@ -0,0 +1,9 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n = int(input())
+print('CY' if n % 7 == 1 or n % 7 == 3 else 'SK')
