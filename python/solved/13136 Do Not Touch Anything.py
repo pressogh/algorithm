9d7fb864150540c567bb65b759ae2de5a9c3c2cd
PRESSO <caff1nepill@gmail.com>
2026-01-23 13:49:26 +0900

    solve: 13136 Do Not Touch Anything

---
+++ python/solved/13136 Do Not Touch Anything.py
@@ -0,0 +1,11 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from math import ceil
+
+r, c, n = map(int, input().split())
+print(ceil(r / n) * ceil(c / n))
