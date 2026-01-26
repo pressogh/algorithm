7bd09a435cd8eaaefddf596d7d1a26957e035e77
PRESSO <caff1nepill@gmail.com>
2026-01-26 12:05:13 +0900

    solve: 14557 Memory

---
+++ python/solved/14557 Memory.py
@@ -0,0 +1,9 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+r, c = map(int, input().split())
+print((r * c) // 2, (r * c) - 1)
