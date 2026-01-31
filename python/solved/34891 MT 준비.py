4fc046e6ca2c03ebf8968d7ebc195b75df133801
PRESSO <caff1nepill@gmail.com>
2026-01-31 14:52:58 +0900

    solve: 34891 MT 준비

---
+++ python/solved/34891 MT 준비.py
@@ -0,0 +1,11 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+from math import ceil
+
+n, m = map(int, input().split())
+print(ceil(n / m))
