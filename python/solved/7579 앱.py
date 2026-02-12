010efa1c1537ab871a09ab503f40e8d7b1463676
PRESSO <caff1nepill@gmail.com>
2026-02-12 16:50:11 +0900

    solve: 7579 앱

--- python/solved/7579 앱.py
+++ python/solved/7579 앱.py
@@ -6,11 +6,11 @@ sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 n, m = map(int, input().split())
-memories, prices = [*map(int, input().split())], [*map(int, input().split())]
+arr = [(memory, price) for memory, price in zip(map(int, input().split()), map(int, input().split()))]
 
 dp = [0] * (10000 + 1)
 for i in range(n):
-    memory, price = memories[i], prices[i]
+    memory, price = arr[i]
     for j in range(10000 - price, -1, -1):
         dp[j + price] = max(dp[j + price], dp[j] + memory)
 
