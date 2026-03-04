899f2b27c31389f41f2b91891ecf365d17e99eca
PRESSO <caff1nepill@gmail.com>
2026-03-04 10:01:34 +0900

    solve: 34945 채플이 너무해

---
+++ python/solved/34945 채플이 너무해.py
@@ -0,0 +1,8 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+print("Oh My God!" if int(input()) < 6 else "Success!")
